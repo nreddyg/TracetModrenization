54d8f3e5022b036ed7f397f7382440b0b0f90e09
Nagendra718 <vvssnmutta@adaequare.com>
2025-11-06 12:07:18 +0530

    User Attributes done

--- src/services/userAttributesServices.ts
+++ src/services/userAttributesServices.ts
@@ -1,4 +1,4 @@
-import { URL_DELETE_CONVERSION, URL_DELETE_ITEM_MASTER, URL_DELETE_STORE, URL_DELETE_UNITS_OF_MEASURE, URL_GET_ITEM_MASTER_DATA, URL_GET_MANAGE_UNITS_OF_MEASURE_DATA, URL_GET_STORE_DATA, URL_GET_UNITS_OF_MEASURE_DATA, URL_GET_UNITS_OF_MEASURE_DATA_BY_ID, URL_GET_USER_ATTRIBUTES, URL_POST_NEW_ITEM_MASTER, URL_POST_NEW_STORE, URL_POST_NEW_UNITS_OF_MEASURE, URL_POST_UPDATE_ITEM_MASTER, URL_POST_UPDATE_STORE, URL_POST_UPDATE_UNITS_OF_MEASURE } from '@/config/apiUrls';
+import { URL_DELETE_CONVERSION, URL_DELETE_GROUP, URL_DELETE_ITEM_MASTER, URL_DELETE_STORE, URL_DELETE_UNITS_OF_MEASURE, URL_GET_ITEM_MASTER_DATA, URL_GET_MANAGE_UNITS_OF_MEASURE_DATA, URL_GET_STORE_DATA, URL_GET_UNITS_OF_MEASURE_DATA, URL_GET_UNITS_OF_MEASURE_DATA_BY_ID, URL_GET_USER_ATTRIBUTES, URL_GET_USER_ATTRIBUTES_DATA_BY_ID, URL_POST_NEW_ITEM_MASTER, URL_POST_NEW_STORE, URL_POST_NEW_UNITS_OF_MEASURE, URL_POST_NEW_USER_ATTRIBUTES, URL_POST_UPDATE_ITEM_MASTER, URL_POST_UPDATE_STORE, URL_POST_UPDATE_UNITS_OF_MEASURE, URL_UPDATE_GROUP } from '@/config/apiUrls';
 import api from './api';
 
 interface APIResponse<T> {
@@ -16,64 +16,39 @@ export const getUserAttributes = async (CompId: string): Promise<APIResponse<any
         return {success: false,message: err.response?.data?.message || err.message,status: err.response?.status};
     }
 }
-export const getEditUOMData = async (CompId: string,id:number | string): Promise<APIResponse<any>> => {
+export const getEditUserAttributeData = async (CompId: string,id:number | string): Promise<APIResponse<any>> => {
     try {
-        const response = await api.get(URL_GET_UNITS_OF_MEASURE_DATA_BY_ID, { params: { CompId: CompId,UOMId:id } })
+        const response = await api.get(URL_GET_USER_ATTRIBUTES_DATA_BY_ID, { params: { CompId: CompId,GroupId:id } })
         return {success: true,data: response.data,}
     } catch (err: any) {
         return {success: false,message: err.response?.data?.message || err.message,status: err.response?.status};
     }
 }
 
-export const deleteUOM = async (compId: string, id: any , data: any): Promise<APIResponse<any>> => {
+export const deleteUserAttribute = async (compId: string, id: any , data: any): Promise<APIResponse<any>> => {
     try {
-        const response = await api.post(URL_DELETE_UNITS_OF_MEASURE, data, { params: { CompId: compId, Id: id } });
+        const response = await api.post(URL_DELETE_GROUP, data, { params: { CompId: compId, GroupId: id } });
         return {success: true,data: response.data};
     } catch (err: any) {
         return {success: false,message: err.response?.data?.message || err.message,status: err.response?.status,};
     }
 };
 
-export const addNewUOM = async (compId: string, data: any): Promise<APIResponse<any>> => {
+export const addNewUserAttribute = async (compId: string, data: any): Promise<APIResponse<any>> => {
     try {
-        const response = await api.post(URL_POST_NEW_UNITS_OF_MEASURE, data, { params: { CompId: compId} });
+        const response = await api.post(URL_POST_NEW_USER_ATTRIBUTES, data, { params: { CompId: compId} });
         return {success: true,data: response.data};
     } catch (err: any) {
         return {success: false,message: err.response?.data?.message || err.message,status: err.response?.status,};
     }
 };
 
-export const updateUOM = async (compId: string, id: any , data: any): Promise<APIResponse<any>> => {
+export const updateUserAttribute = async (compId: string, id: any , data: any): Promise<APIResponse<any>> => {
     try {
-        const response = await api.post(URL_POST_UPDATE_UNITS_OF_MEASURE, data, { params: { CompId: compId, UOMId: id } });
+        const response = await api.post(URL_UPDATE_GROUP, data, { params: { CompId: compId, GroupId: id } });
         return {success: true,data: response.data};
     } catch (err: any) {
         return {success: false,message: err.response?.data?.message || err.message,status: err.response?.status,};
     }
 };
 
-
-export const getConversionUOMData = async (CompId: string): Promise<APIResponse<any>> => {
-    try {
-        const response = await api.get(URL_GET_MANAGE_UNITS_OF_MEASURE_DATA, { params: { CompId: CompId } })
-        return {success: true,data: response.data,}
-    } catch (err: any) {
-        return {success: false,message: err.response?.data?.message || err.message,status: err.response?.status};
-    }
-}
-export const deleteConversion = async (compId: string, id: any , data: any): Promise<APIResponse<any>> => {
-    try {
-        const response = await api.post(URL_DELETE_CONVERSION, data, { params: { CompId: compId, UnitConversionId: id } });
-        return {success: true,data: response.data};
-    } catch (err: any) {
-        return {success: false,message: err.response?.data?.message || err.message,status: err.response?.status,};
-    }
-};
-export const addNewConversion = async (compId: string, data: any): Promise<APIResponse<any>> => {
-    try {
-        const response = await api.post(URL_POST_NEW_UNITS_OF_MEASURE, data, { params: { CompId: compId} });
-        return {success: true,data: response.data};
-    } catch (err: any) {
-        return {success: false,message: err.response?.data?.message || err.message,status: err.response?.status,};
-    }
-};

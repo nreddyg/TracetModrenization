6e22e1da88714ffe6cc70ad25bc0f98bfbd6bcca
Ganesh Kalyanam <gkalyanam@adaequare.com>
2025-11-14 12:28:20 +0530

    ui changes

--- src/pages/masters/Vendor.tsx
+++ src/pages/masters/Vendor.tsx
@@ -512,7 +512,7 @@ const Vendor = () => {
         {/* Main Content Area */}
         <div className="flex-1 flex flex-col min-w-0 ">
           {/* Navigation and Action Bar */}
-          <div className="bg-white border-b shadow-sm px-4 lg:px-6 py-3 flex flex-row xxs:flex-col xs2:flex-row lg:flex-row lg:items-center justify-between gap-4 shrink-0">
+          <div className="px-4 lg:px-6 py-4 flex flex-row xxs:flex-col xs2:flex-row lg:flex-row lg:items-center justify-between gap-4 shrink-0">
             <div className="flex items-center gap-4 lg:gap-6 flex-1 min-w-0">
               <div className="flex items-center gap-2">
                 <div className="flex items-center gap-2 text-sm text-gray-600">
@@ -549,7 +549,7 @@ const Vendor = () => {
           </div>
 
           {/* Content Grid with Individual Scroll Areas */}
-          <div className="flex-1 p-3 overflow-hidden min-h-0  ">
+          <div className="flex-1 p-3 pt-0 overflow-hidden min-h-0  ">
             <div className="grid grid-cols-1 lg:grid-cols-12 gap-1 h-full">
               {/* Left Column - Main Content */}
               <div className="lg:col-span-12 flex flex-col  min-h-0 ">

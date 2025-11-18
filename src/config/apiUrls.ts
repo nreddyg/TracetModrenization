e367efed7c81d378a7f2eb8af36e9e8e16ca05c2
Ganesh Kalyanam <gkalyanam@adaequare.com>
2025-11-18 12:16:44 +0530

    lookups add asset/ edit asset urls configured

--- src/config/apiUrls.ts
+++ src/config/apiUrls.ts
@@ -283,6 +283,16 @@ export const URL_GRID_COLS = `/api/FixedAssetAPI/GetAllGridColumnsList`;
 
 //Fixed Assets Module
 //Lookups
-export const URL_GET_SELLER_LOOKUP_DATA='api/Masters/GetSellerLookups';
-// export const UR
-//Add Asset
+export const URL_GET_SELLER_LOOKUP_DATA='/api/Masters/GetSellerLookups';
+export const URL_GET_ACQUISITION_TYPE_LIST='/api/Masters/GetAcquisitionTypeLookups';
+export const URL_GET_DEPENDENCY_TYPE_LIST='/api/Masters/GetDependencyTypeLookups';
+export const URL_GET_WORKING_CONDITION_LOOKUP_LIST='/api/Masters/GetWorkingConditionLookups';
+export const URL_GET_DEPARTMENT_LOOKUP_DATA_BY_USER='/api/Masters/GetDepartmentDetailsByUser';
+export const URL_GET_MANUFACTURER_LOOKUP_DATA='/api/Masters/GetManufacturerLookups';
+export const URL_GET_ASSET_TAGGABLE_LOOKUP_DATA='/api/Masters/GetIsAssetTaggableLookups';
+export const URL_GET_ASSIGNED_TO_LOOKUP_DATA='/api/Masters/GetAssignedUserLookups';
+export const URL_GET_ASSET_OWNER_LOOKUP_DATA='/api/Masters/GetAssetOwnerLookups';
+//Add Asset or Edit Asset Details
+export const URL_GET_USER_ATTRIBUTES_BY_SUB_CAT='/api/FixedAssetAPI/GetUserAttributesByCategoryId';
+export const URL_GET_COST_BREAKUP_DATA_SUB_CAT='/api/FixedAssetAPI/GetCostBreakupByCatId';
+export const URL_GET_ASSET_DETAILS='/api/FixedAssetAPI/GetManageAssetsListWithAttributesById'
